5e130f8a266809ba5df0cc95364b03806903ce61
Xolani <xzchauke@gmail.com>
2022-11-30 03:37:16 +0200

    adjusted functions

--- models/base_model.py
+++ models/base_model.py
@@ -10,39 +10,21 @@ class BaseModel:
 
     """ Represents the BaseModel class for the project."""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes a the BaseModel
+    def __init__(self):
+        """Initializes a the BaseModel """
 
-        Args:
-            - *args: the entered args.
-            - **kwargs (dict): key/value of the args
-        """
-        tmeform = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if len(kwargs) != 0 and kwargs != {}:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, tmeform)
-                else:
-                    self.__dict__[key] = value
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns string representation of the class name, id, dict"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates updated_at attribute with the current time"""
 
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values"""
